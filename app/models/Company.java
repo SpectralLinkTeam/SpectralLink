9fd3dc23859770c3019ea3e4ed13db09a0ede251
Nikola Obradović <obradovic.nix@gmail.com>
2016-11-20 00:47:54 +0100

    Klasa Preduzece.

--- app/models/Company.java
+++ app/models/Company.java
@@ -3,5 +3,29 @@ package models;
 import play.db.jpa.Model;
 
 public class Company extends Model {
+	//naziv
+	public String name;
+	//adresa
+	public String address;
+	public String PIB;
+	//telefon
+	public String telephone;
+	public String email;
+	public String logo;
+	
+	public Company(){
+		super();
+	}
+
+	public Company(String naziv, String adresa, String pIB, String telefon,
+			String email, String logo) {
+		super();
+		this.name = naziv;
+		this.address = adresa;
+		PIB = pIB;
+		this.telephone = telefon;
+		this.email = email;
+		this.logo = logo;
+	}
 
 }
