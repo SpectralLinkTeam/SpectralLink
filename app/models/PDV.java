b57bdbc232ac6cc3711ada5ee03718c5c81fc5be
Nikola Obradović <obradovic.nix@gmail.com>
2016-11-20 11:42:53 +0100

    Klase za PDV i Stopu PDVa #1

---
+++ app/models/PDV.java
@@ -0,0 +1,11 @@
+package models;
+
+import play.db.jpa.Model;
+
+public class PDV extends Model {
+	
+	public String naziv;
+	
+	public StopaPDV stopaPDV;
+
+}
---
+++ app/models/StopaPDV.java
@@ -0,0 +1,13 @@
+package models;
+
+import java.util.Date;
+
+import play.db.jpa.Model;
+
+public class StopaPDV extends Model {
+	
+	public int procenat;
+	
+	public Date datumVazenja;
+
+}
