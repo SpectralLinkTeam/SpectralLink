682484be676907191ceb0057c3593fa6bb34c0f0
Nikola Obradović <obradovic.nix@gmail.com>
2017-03-06 18:01:22 +0100

    Nova klasa Narudzbenica.

---
+++ app/models/Narudzbenica.java
@@ -0,0 +1,21 @@
+package models;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+
+import play.db.jpa.Model;
+
+@Entity
+public class Narudzbenica extends Model {
+	
+	@OneToOne(mappedBy="narudzbenica",  cascade = {CascadeType.ALL})
+	public Company dobavljac;
+
+	@OneToOne(mappedBy="narudzbenica",  cascade = {CascadeType.ALL})
+	public Company kupac;
+	
+	//== stavke narudzbenice
+	
+}
