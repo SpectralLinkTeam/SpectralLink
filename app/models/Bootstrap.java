6720bafcb2e1ec197f152e0d929d327ef3347dc4
alligator <stefanosengl@gmail.com>
2017-03-07 19:54:04 +0100

    Bug finished

--- app/models/Bootstrap.java
+++ app/models/Bootstrap.java
@@ -8,11 +8,11 @@ import models.*;
 @OnApplicationStart
 public class Bootstrap extends Job {
     public void doJob() {
-    /*
+
         // Check if the database is empty
         if(Company.count() == 0) {
             Fixtures.loadModels("initial-data.yml");
         }
-        */
+
     }
 }
