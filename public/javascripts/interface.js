552acba47a9af35fbca7638244781df8f6f9c87b
alligator <stefanosengl@gmail.com>
2017-03-16 17:40:06 +0100

    Modal windows fix

--- public/javascripts/interface.js
+++ public/javascripts/interface.js
@@ -8,7 +8,7 @@
     
     $("#poslovna-godina-admin").click(function () {
         $("#modal-container").empty();
-        $("#modal-container").load("modal-poslovna-godina.html");
+        $("#modal-container").load("/modal-poslovna-godina.html");
         $("#modal-container").css("display", "block");
         $(".overlay").show();
     });
@@ -16,7 +16,7 @@
     
     $("#pdv-admin").click(function () {
         $("#modal-container").empty();
-        $("#modal-container").load("modal-pdv.html");
+        $("#modal-container").load("/modal-pdv.html");
         $("#modal-container").css("display", "block");
         $(".overlay").show();
     });
@@ -43,24 +43,16 @@
     
     $(document).on("click", "#add-new-product", function () {
         $("#modal-container").empty();
-        $("#modal-container").load("add-new-product-form.html");
+        $("#modal-container").load("/add-new-product-form.html");
         $("#modal-container").css("display", "block");
         $(".overlay").show();
     });
     
     $(document).on("click", "#add-new-partner", function () {
         $("#modal-container").empty();
-        $("#modal-container").load("add-new-partner-form.html");
+        $("#modal-container").load("/add-new-partner-form.html");
         $("#modal-container").css("display", "block");
         $(".overlay").show();
-    });
-    
-    
-    // HORIZONTAL MENU
-    
-    $("#notif-narudzbenice").click(function () {
-        $("section").empty();
-        $("section").load("table-narudzbenice.html");
     });
     
     
@@ -68,7 +60,7 @@
     
     $(document).on("click", ".product-list-link", function () {
         $("#modal-container").empty();
-        $("#modal-container").load("view-product-list.html");
+        $("#modal-container").load("/view-product-list.html");
         $("#modal-container").css("display", "block");
         $(".overlay").show();
     });
