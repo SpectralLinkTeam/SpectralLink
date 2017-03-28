b8ba2e9e9f8011228a40c7f62dc6705d4c75779e
Nikola Obradović <obradovic.nix@gmail.com>
2017-03-28 23:52:36 +0200

    Mesto controller java klasa

--- app/controllers/MestoController.java
+++ app/controllers/MestoController.java
@@ -1,7 +1,36 @@
 package controllers;
 
+import java.util.List;
+
+import models.Mesto;
 import play.mvc.Controller;
 
 public class MestoController extends Controller {
-// crdu i search
+	
+// crud i search
+	
+	public static void show(){
+	    List<Mesto> pp = Mesto.findAll();
+        renderTemplate("Dobavljac/Mesto/show.html", pp);
+    }
+	
+	public static void add(Mesto mesto){
+		mesto.save();
+	    show();
+    }
+	
+	public static void edit(Mesto mesto){
+		mesto.save();
+		show();
+	}
+	//-- fizicko brisanje
+	public static void delete(long id){
+		Mesto mesto = Mesto.findById(id);
+		mesto.delete();
+		show();
+	}
+	
+	public static void search(String searchTerm){
+		
+	}
 }
