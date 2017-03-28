87f39f3dd65125fc49c29d45400f3ff6eca5e530
Nikola Obradović <obradovic.nix@gmail.com>
2017-03-28 23:52:17 +0200

    Business year controller java klasa

--- app/controllers/BusinessYearController.java
+++ app/controllers/BusinessYearController.java
@@ -1,8 +1,37 @@
 package controllers;
 
+import java.util.List;
+
+import models.BusinessYear;
 import play.mvc.Controller;
 
 public class BusinessYearController extends Controller {
 
 	//crud i search
+	
+	public static void show(){
+	    List<BusinessYear> pp = BusinessYear.findAll();
+        renderTemplate("Dobavljac/BusinessYear/show.html", pp);
+    }
+	
+	public static void add(BusinessYear poslovnaGodina){
+		poslovnaGodina.save();
+	    show();
+    }
+	
+	public static void edit(BusinessYear poslovnaGodina){
+		poslovnaGodina.save();
+		show();
+	}
+	
+	public static void complete(long id){
+		BusinessYear poslovnaGodina = BusinessYear.findById(id);
+		poslovnaGodina.completed = true;
+		poslovnaGodina.save();
+		show();
+	}
+	
+	public static void search(String searchTerm){
+		
+	}
 }
