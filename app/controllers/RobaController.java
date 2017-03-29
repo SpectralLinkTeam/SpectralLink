fd4983618943c72303882d19fb54da3adcebee9b
Nikola Obradović <obradovic.nix@gmail.com>
2017-03-29 17:59:34 +0200

    RobaController

--- app/controllers/RobaController.java
+++ app/controllers/RobaController.java
@@ -1,7 +1,38 @@
 package controllers;
 
+import java.util.List;
+
+import models.Roba;
 import play.mvc.Controller;
 
 public class RobaController extends Controller {
 // crud i search
+	
+	public static void show(){
+	    List<Roba> lista = Roba.find("byIsDeleted", "0").fetch();
+        renderTemplate("Dobavljac/Roba/show.html", lista);
+    }
+	
+	public static void add(Roba roba){
+		roba.save();
+	    show();
+    }
+	
+	public static void edit(Roba roba){
+		roba.save();
+		show();
+	}
+	
+	public static void delete(long id){
+	    //logicko brisanje
+		Roba roba = Roba.findById(id);
+		roba.IsDeleted = true;
+		roba.save();
+		show();
+	}
+	
+	public static void search(String searchTerm){
+		List<Roba> lista = Roba.find("byIsDeletedAndNazivLikeAndOpisLike", "0", "%"+searchTerm+"%", "%"+searchTerm+"%").fetch();
+        renderTemplate("Dobavljac/Roba/show.html", lista);
+	}
 }
