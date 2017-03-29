a017b7d33c965ce462a0f6317537a8658dcbd06f
Nikola Obradović <obradovic.nix@gmail.com>
2017-03-29 17:56:48 +0200

    PDVController

--- app/controllers/PDVController.java
+++ app/controllers/PDVController.java
@@ -1,7 +1,35 @@
 package controllers;
 
+import java.util.List;
+
+import models.PDV;
 import play.mvc.Controller;
 
 public class PDVController extends Controller {
 // crud i search
+	public static void show(){
+	    List<PDV> lista = PDV.findAll();
+        renderTemplate("Dobavljac/PDV/show.html", lista);
+    }
+	
+	public static void add(PDV pdv){
+		pdv.save();
+	    show();
+    }
+	
+	public static void edit(PDV pdv){
+		pdv.save();
+		show();
+	}
+	//-- fizicko brisanje
+	public static void delete(long id){
+		PDV pdv = PDV.findById(id);
+		pdv.delete();
+		show();
+	}
+	
+	public static void search(String searchTerm){
+		List<PDV> lista = PDV.find("byNaziv", "%" + searchTerm + "%").fetch();
+        renderTemplate("Dobavljac/PDV/show.html", lista);
+	}
 }
