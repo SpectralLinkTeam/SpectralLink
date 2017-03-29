0c3f80576be6fdec715bdc114e956a37fee44013
Nikola Obradović <obradovic.nix@gmail.com>
2017-03-29 18:05:11 +0200

    GrupaRobeController metode.

--- app/controllers/GrupaRobeController.java
+++ app/controllers/GrupaRobeController.java
@@ -1,7 +1,38 @@
 package controllers;
 
+import java.util.List;
+
+import models.GrupaRobe;
 import play.mvc.Controller;
 
 public class GrupaRobeController extends Controller {
 // crud i search
+	
+	public static void show(){
+	    List<GrupaRobe> lista = GrupaRobe.find("byIsDeleted", "0").fetch();
+        renderTemplate("Dobavljac/GrupaRobe/show.html", lista);
+    }
+	
+	public static void add(GrupaRobe gr){
+		gr.save();
+	    show();
+    }
+	
+	public static void edit(GrupaRobe gr){
+		gr.save();
+		show();
+	}
+	
+	public static void delete(long id){
+	    //logicko brisanje
+		GrupaRobe gr = GrupaRobe.findById(id);
+		gr.IsDeleted = true;
+		gr.save();
+		show();
+	}
+	
+	public static void search(String searchTerm){
+		List<GrupaRobe> lista = GrupaRobe.find("byIsDeletedAndNazivLike", "0", "%"+searchTerm+"%").fetch();
+        renderTemplate("Dobavljac/GrupaRobe/show.html", lista);
+	}
 }
