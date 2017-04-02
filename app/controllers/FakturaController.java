09846caa54444e7162e82254bc151933aa712602
Nikola Nikolas <tojaa92@gmail.com>
2017-04-02 22:02:21 +0200

    prikaz novih narudzbenica

--- app/controllers/FakturaController.java
+++ app/controllers/FakturaController.java
@@ -1,11 +1,17 @@
 package controllers;
 
+import java.util.List;
+
+import models.Narudzbenica;
 import play.mvc.Controller;
+import viewmodels.NarudzbenicaViewModel;
 
 public class FakturaController extends Controller {
 //crud i search
 	// generisanje fakture na osnovu narudzbenice
-	
+	public static void generisiFakturu(long id){
+		render();
+	}
 	
 	// ..... fakture koje su zakljucene se ne mogu menjati - READONLY
 	
--- app/controllers/NarudzbenicaController.java
+++ app/controllers/NarudzbenicaController.java
@@ -1,11 +1,22 @@
 package controllers;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import models.Cenovnik;
+import models.GrupaRobe;
 import models.Narudzbenica;
+import models.PDV;
+import models.ShoppingCartItems;
+import models.StavkaCenovnika;
+import models.StopaPDV;
 import play.mvc.Controller;
 import viewmodels.NarudzbenicaViewModel;
+import viewmodels.ShoppingCartViewModel;
 
 public class NarudzbenicaController extends Controller {
 // crud i search
@@ -13,10 +24,17 @@ public class NarudzbenicaController extends Controller {
 	// da li je od nje generisana faktura ili nije
 	// ako je generisana - read only
 	
+	public static void noveNarudzbenice(){
+		List<Narudzbenica> narudzbenice = Narudzbenica.find("byFakturaGenerisanaAndIsDeleted", false,0).fetch();
+		NarudzbenicaViewModel narudzbeniceViewModel = NarudzbenicaController.narudzbenice();
+		renderTemplate("Dobavljac/narudzbenice.html", narudzbenice, narudzbeniceViewModel);
+	}
 	
 	public static NarudzbenicaViewModel narudzbenice(){
 		int sveNarudzbenice = Narudzbenica.find("byIsDeleted",0).fetch().size();
 		int noveNarudzbenice = Narudzbenica.find("byFakturaGenerisanaAndIsDeleted", false,0).fetch().size();
 		return new NarudzbenicaViewModel(sveNarudzbenice, noveNarudzbenice);
 	}
+	
+	
 }
